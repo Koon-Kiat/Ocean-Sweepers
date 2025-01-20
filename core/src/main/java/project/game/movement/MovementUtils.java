62a2adbb933561a6fb9e58de5f7584d80030b3a5
Koon Kiat <bookoonkiat@gmail.com>
2025-01-20 23:07:49 +0800

    Add MovementUtils class for position clamping and diagonal speed calculation

---
+++ core/src/main/java/project/game/movement/MovementUtils.java
@@ -0,0 +1,29 @@
+package project.game.movement;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+import project.game.Main;
+
+public class MovementUtils {
+
+    /**
+     * Clamps the position within the game boundaries.
+     *
+     * @param position The position vector to clamp.
+     */
+    public static void clampPosition(Vector2 position) {
+        position.x = MathUtils.clamp(position.x, 0, Main.GAME_WIDTH);
+        position.y = MathUtils.clamp(position.y, 0, Main.GAME_HEIGHT);
+    }
+
+    /**
+     * Calculates the diagonal speed to maintain consistent overall speed.
+     *
+     * @param speed The current speed.
+     * @return The diagonal speed.
+     */
+    public static float calculateDiagonalSpeed(float speed) {
+        return speed / (float) Math.sqrt(2);
+    }
+}
