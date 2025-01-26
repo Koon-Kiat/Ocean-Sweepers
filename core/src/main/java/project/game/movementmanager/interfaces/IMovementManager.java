37fa1e9282c903c9b77597ebc2cdb504ed48bd12
Koon Kiat <bookoonkiat@gmail.com>
2025-01-26 15:44:22 +0800

    Formating

--- core/src/main/java/project/game/movementmanager/interfaces/IMovementManager.java
+++ core/src/main/java/project/game/movementmanager/interfaces/IMovementManager.java
@@ -3,9 +3,14 @@ package project.game.movementmanager.interfaces;
 import project.game.movementmanager.Direction;
 
 public interface IMovementManager {
+
     void setDeltaTime(float deltaTime);
+
     void updateMovement();
+
     float getX();
+
     float getY();
+
     void setDirection(Direction direction);
 }
