4a2ac89f7d16a8b017753b2aef5d09c5c07bf44b
Koon Kiat <bookoonkiat@gmail.com>
2025-02-07 21:15:05 +0800

    Update interface to woirk with MovementData

---
+++ core/src/main/java/project/game/abstractengine/movementmanager/interfaces/IMovementBehavior.java
@@ -0,0 +1,23 @@
+package project.game.abstractengine.movementmanager.interfaces;
+
+import project.game.abstractengine.movementmanager.MovementData;
+
+/**
+ * @interface IMovementBehavior
+ * @brief Defines the contract for movement behaviors.
+ *
+ * Classes implementing this interface must provide an implementation for
+ * updating the position of a MovementManager. This allows for flexible movement
+ * patterns such as constant movement, accelerated movement, and zig-zag
+ * movement.
+ */
+public interface IMovementBehavior {
+
+    /**
+     * Updates the position of the given MovementManager based on the specific
+     * behavior.
+     *
+     * @param manager The MovementManager whose position needs to be updated.
+     */
+    void updatePosition(MovementData movementData);
+}
