51896e5dbcd81a3906f3a093d2d23a016b1bc875
Koon Kiat <bookoonkiat@gmail.com>
2025-02-07 21:17:57 +0800

    Remove clamp

--- core/src/main/java/project/game/abstractengine/movementmanager/MovementUtils.java
+++ core/src/main/java/project/game/abstractengine/movementmanager/MovementUtils.java
@@ -1,22 +1,6 @@
 package project.game.abstractengine.movementmanager;
 
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
-
-import project.game.Main;
-
 public class MovementUtils {
-
-    /**
-     * Clamps the position within the game boundaries.
-     *
-     * @param position The position vector to clamp.
-     */
-    public static void clampPosition(Vector2 position) {
-        position.x = MathUtils.clamp(position.x, 0, Main.GAME_WIDTH);
-        position.y = MathUtils.clamp(position.y, 0, Main.GAME_HEIGHT);
-    }
-
     /**
      * Calculates the diagonal speed to maintain consistent overall speed.
      *
