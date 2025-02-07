5eaa85859c8b2d4dd1bdbd349920e1e55e180fc7
Koon Kiat <bookoonkiat@gmail.com>
2025-02-07 21:18:13 +0800

    Move Directions to outside of abstractengine package

---
+++ core/src/main/java/project/game/Direction.java
@@ -0,0 +1,18 @@
+package project.game;
+
+public enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP_LEFT,
+    UP_RIGHT,
+    DOWN_LEFT,
+    DOWN_RIGHT,
+    NONE;
+
+
+    public boolean is(Direction direction) {
+        return this == direction;
+    }
+}
