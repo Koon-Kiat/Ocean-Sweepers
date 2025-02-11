6bedb0deae7b7e5c4ad04d2cb21b009f6d2afef5
Koon Kiat <bookoonkiat@gmail.com>
2025-02-12 01:38:58 +0800

    Refactor movement interfaces to improve method naming; replace updatePosition with applyMovementBehavior for clarity and enhance documentation formatting.

--- core/src/main/java/project/game/abstractengine/movementmanager/interfaces/IMovementBehavior.java
+++ core/src/main/java/project/game/abstractengine/movementmanager/interfaces/IMovementBehavior.java
@@ -8,16 +8,10 @@ import project.game.abstractengine.movementmanager.MovementData;
  *
  *        Classes implementing this interface must provide an implementation for
  *        updating the position of a MovementManager. This allows for flexible
- *        movement
- *        patterns such as constant movement, accelerated movement, and zig-zag
- *        movement.
+ *        movement patterns such as constant movement, accelerated movement, and
+ *        zig-zag movement.
  */
 public interface IMovementBehavior {
 
-    /**
-     * Updates the position using MovementData.
-     *
-     * @param movementData Data needed to update the position.
-     */
-    void updatePosition(MovementData movementData);
+    void applyMovementBehavior(MovementData movementData);
 }
