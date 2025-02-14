59e149ae886a4dd7f8f4863029f9440733aea7a4
Koon Kiat <bookoonkiat@gmail.com>
2025-02-14 23:26:32 +0800

    Refactor movement behaviors to use MovableEntity for movement data management

--- core/src/main/java/project/game/defaultmovements/ConstantMovementBehavior.java
+++ core/src/main/java/project/game/defaultmovements/ConstantMovementBehavior.java
@@ -5,8 +5,8 @@ import java.util.logging.Logger;
 
 import com.badlogic.gdx.math.Vector2;
 
+import project.game.abstractengine.entitysystem.entitymanager.MovableEntity;
 import project.game.abstractengine.entitysystem.interfaces.IMovementBehavior;
-import project.game.abstractengine.entitysystem.movementmanager.MovementData;
 import project.game.abstractengine.entitysystem.movementmanager.MovementUtils;
 import project.game.exceptions.MovementException;
 
@@ -39,10 +39,10 @@ public class ConstantMovementBehavior implements IMovementBehavior {
     }
 
     @Override
-    public void applyMovementBehavior(MovementData data, float deltaTime) {
+    public void applyMovementBehavior(MovableEntity entity, float deltaTime) {
         try {
             Vector2 deltaMovement = new Vector2();
-            switch (data.getDirection()) {
+            switch (entity.getDirection()) {
                 case UP:
                     deltaMovement.y += speed * deltaTime;
                     break;
@@ -75,12 +75,12 @@ public class ConstantMovementBehavior implements IMovementBehavior {
                     break;
                 default:
                     String errorMessage = "Unknown direction in ConstantMovementBehavior.updatePosition: "
-                            + data.getDirection();
+                            + entity.getDirection();
                     LOGGER.log(Level.SEVERE, errorMessage);
                     throw new MovementException(errorMessage);
             }
-            data.setX(data.getX() + deltaMovement.x);
-            data.setY(data.getY() + deltaMovement.y);
+            entity.setX(entity.getX() + deltaMovement.x);
+            entity.setY(entity.getY() + deltaMovement.y);
         } catch (MovementException e) {
             LOGGER.log(Level.SEVERE, "Illegal argument in ConstantMovementBehavior.updatePosition: " + e.getMessage(),
                     e);
