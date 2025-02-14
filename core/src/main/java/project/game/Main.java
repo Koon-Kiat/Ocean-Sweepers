0d85e4eaaaff8830ce6a298524372cbbd032870e
Koon Kiat <bookoonkiat@gmail.com>
2025-02-14 19:48:56 +0800

    Refactor import statements in Main.java and moved scenemanager to abstractengine package

--- core/src/main/java/project/game/Main.java
+++ core/src/main/java/project/game/Main.java
@@ -16,10 +16,10 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import project.game.abstractengine.entity.movementmanager.interfaces.IMovementBehavior;
 import project.game.abstractengine.entity.movementmanager.interfaces.IMovementManager;
 import project.game.abstractengine.iomanager.SceneIOManager;
+import project.game.abstractengine.scenemanager.GameScene;
+import project.game.abstractengine.scenemanager.MainMenuScene;
+import project.game.abstractengine.scenemanager.SceneManager;
 import project.game.logmanager.LogManager;
-import project.game.scenemanager.GameScene;
-import project.game.scenemanager.MainMenuScene;
-import project.game.scenemanager.SceneManager;
 
 
 public class Main extends ApplicationAdapter {
