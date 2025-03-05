e8e83e01a619dc8de02a5175c10112be6c9b11cc
Koon Kiat <bookoonkiat@gmail.com>
2025-03-06 00:23:47 +0800

    Add MovementStrategyDecorator to enhance movement strategy functionality

---
+++ core/src/main/java/project/game/context/decorator/MovementStrategyDecorator.java
@@ -0,0 +1,21 @@
+package project.game.context.decorator;
+
+import project.game.engine.api.movement.IMovable;
+import project.game.engine.api.movement.IMovementStrategy;
+
+public abstract class MovementStrategyDecorator implements IMovementStrategy {
+    protected final IMovementStrategy wrappedStrategy;
+
+    public MovementStrategyDecorator(IMovementStrategy strategy) {
+        this.wrappedStrategy = strategy;
+    }
+
+    @Override
+    public void move(IMovable movable, float deltaTime) {
+        wrappedStrategy.move(movable, deltaTime);
+    }
+
+    public IMovementStrategy getWrappedStrategy() {
+        return wrappedStrategy;
+    }
+}
